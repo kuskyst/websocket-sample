e2f4d3ebe7f30ae88d501b9be5d8f74a6493e6dc
kuskyst <126965999+kuskyst@users.noreply.github.com>
2024-11-20 19:03:46 +0900

    first

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kuskyst/websocket-sample
+
+go 1.23.0
+
+require github.com/gorilla/websocket v1.5.3
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+
+	log.Println("Client connected")
+
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", echo)
+	fmt.Println("Server is running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
